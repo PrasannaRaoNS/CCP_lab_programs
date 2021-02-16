01762ea79766fb829ba2933ba80c1b5997a0ff74
NS Prasanna Rao <78875520+PrasannaRaoNS@users.noreply.github.com>
2021-02-16 09:44:02 +0530

    Add files via upload
    Program to print even numbers from M to N.

---
+++ even no's.c
@@ -0,0 +1,21 @@
+/*program to print even numbers from M to N.*/
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i,a,b;
+    printf("Enter the range\n");
+    scanf("%d%d",&M,&N);
+
+    printf("Even numbers from %d to %d = \n",M,N);
+    for(i=M; i<=N; i++)
+    {
+        if(i%2 == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
